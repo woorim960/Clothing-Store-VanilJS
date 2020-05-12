a5825ca1b94dce78181878c49151543c47548bbb
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-05-12 19:55:49 +0900

    코드별 주석 추가

--- js/index.js
+++ js/index.js
@@ -1,31 +1,38 @@
 'use strict';
 
+// 로고버튼과 네비게이션 버튼, 옷 리스트들을 담은 변수
 const logoBtn = document.querySelector('.main-title .logo'),
   clothingBtns = document.querySelectorAll('.main-title .nav .clothing-btn'),
   clothingList = Array.from(document.querySelectorAll('.main-view .clothing-list'))[0].children;
 
-function reload() {
-  location.reload();
-}
-
+// 선택된 버튼에 맞춰 옷이 조회되도록 하는 함수
 function showSlectedClothing(event) {
   const btn = event.target;
   
+  // 모든 리스트 순회
   for (let clothing of clothingList) {
     let img = clothing.childNodes[1];
+    
     if (img.classList.contains(btn.id)) {
+      // 이미지 클래스에 클릭된 버튼의 id가 있으면 "deactive" 클래스를 지워줌으로써 "display=none"속성을 없애줌.
       clothing.classList.remove('deactivate');
     } else {
+      // 이미지 클래스에 클릭된 버튼의 id가 없으면 "deactive" 클래스를 넣어줌으로써 "display=none"속성이 추가됨.
       clothing.classList.add('deactivate');
     }
   }
 }
 
-function init() {
+// 메인 함수
+function main() {
+  // 로고 버튼 클릭시 새로고침 이벤트 발생 => 원래 classList.remove로 "deactive" 클래스를 지워주는 것으로 했는데 작동이 안해서 새로고침으로라도 했습니다...ㅠ
   logoBtn.addEventListener('click', reload);
+
+  // 옷 버튼 클릭시 선택된 옷들만 분류하여 조회하는 함수 실행
   Array.from(clothingBtns).forEach(clothingBtn => {
     addEventListener('click', showSlectedClothing);
   });
 }
 
-init();
+// 메인 함수 호출
+main();
